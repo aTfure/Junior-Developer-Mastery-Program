791ccdb180ef8e1ba466f9dbed96e81a0840b21c
aTfure <atfure@gmail.com>
2024-04-15 16:05:18 +0300

    Update login route and add styles for login page

--- instagram/instagram/src/App.js
+++ instagram/instagram/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 // import * as ROUTES from "./constants/routes";
 
 const Login = lazy(() => import("./pages/login"));
+const SignUp = lazy(() => import("./pages/sign-up"));
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route>
           <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
       </Route>
       </Routes>
       </Suspense>
--- instagram/instagram/tailwind.config.js
+++ instagram/instagram/tailwind.config.js
@@ -2,7 +2,28 @@
 module.exports = {
   content: ["./src/pages/login.js"],
   theme: {
+    fill: (theme) => ({
+      red: theme("colors.red.primary"),
+    }),
+    colors: {
+      white: "#ffffff",
+      blue: {
+        medium: "#005c98",
+      },
+      black: {
+        light: "#005c98",
+        faded: "#00000059",
+      },
+      gray: {
+        base: "#616161",
+        background: "#fafafa",
+        primary: "#dbdbdb",
+      },
+      red: {
+        primary: "#ed4956",
+      },
+    },
     extend: {},
   },
   plugins: [],
-}
+};
