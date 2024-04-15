9ce17ecfbbe23580142e9aa5294b74949585453e
aTfure <atfure@gmail.com>
2024-04-15 12:34:18 +0300

    Update Tailwind CSS configuration and styles for Instagram app

--- instagram/instagram/src/App.js
+++ instagram/instagram/src/App.js
@@ -1,6 +1,6 @@
 import { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import * as ROUTES from "./constants/routes";
+// import * as ROUTES from "./constants/routes";
 
 const Login = lazy(() => import("./pages/login"));
 
