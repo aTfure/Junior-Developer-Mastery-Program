4f039a6eb08c9826797f2733e2d4848567126088
aTfure <atfure@gmail.com>
2024-04-16 15:53:49 +0300

    Update sidebar component and user.js to use useContext and add propTypes

--- instagram/instagram/src/components/sidebar/index.js
+++ instagram/instagram/src/components/sidebar/index.js
@@ -1,15 +1,17 @@
-import useUser from "../../hooks/use-user";
-import User from "./user";
-import Suggestions from "./suggestions";
-
+import { useContext } from 'react';
+import User from './user';
+import Suggestions from './suggestions';
+import LoggedInUserContext from '../../context/logged-in-user';
 
 export default function Sidebar() {
-    const {
-        user: { fullName, username, userId}
-    } = useUser();
+  const { user: { docId = '', fullName, username, userId, following } = {} } = useContext(
+    LoggedInUserContext
+  );
 
-    return <div className="p-4">
-        <User username={username} fullname={fullName} />
-        <Suggestions userId={userId} />
+  return (
+    <div className="p-4">
+      <User username={username} fullName={fullName} />
+      <Suggestions userId={userId} following={following} loggedInUserDocId={docId} />
     </div>
+  );
 }
--- instagram/instagram/src/pages/dashboard.js
+++ instagram/instagram/src/pages/dashboard.js
@@ -1,21 +1,32 @@
 import { useEffect } from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/header';
-import Sidebar from '../components/sidebar';
 import Timeline from '../components/timeline';
+import Sidebar from '../components/sidebar';
+import useUser from '../hooks/use-user';
+import LoggedInUserContext from '../context/logged-in-user';
 
+export default function Dashboard({ user: loggedInUser }) {
+  const userHookResult = useUser(loggedInUser ? loggedInUser.uid : null);
+  const { user, setActiveUser } = userHookResult;
 
-export default function Dashboard() {
-    useEffect(() => {
-        document.title = 'Instagram';
-    }, []);
+  useEffect(() => {
+    document.title = "Instagram";
+  }, []);
 
-    return (
-        <div className="bg-gray-background">
-            <Header />
-            <div className='grid grid-cols-3 gap-4 justify-between mx-auto max-w-screen-lg'>
-                <Timeline />
-                <Sidebar />
-            </div>
+  return (
+    <LoggedInUserContext.Provider value={{ user, setActiveUser }}>
+      <div className="bg-gray-background">
+        <Header />
+        <div className="grid grid-cols-3 gap-4 justify-between mx-auto max-w-screen-lg">
+          <Timeline />
+          <Sidebar />
         </div>
-    )
+      </div>
+    </LoggedInUserContext.Provider>
+  );
 }
+
+Dashboard.propTypes = {
+  user: PropTypes.object.isRequired
+};
