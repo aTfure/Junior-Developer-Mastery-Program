ea93e0f1d8c404f776c956c94c10f12d19ec31cf
aTfure <atfure@gmail.com>
2024-04-17 06:54:01 +0300

    Add new timeline component and update App.js to display user's photos

--- instagram/instagram/src/components/timeline.js
+++ instagram/instagram/src/components/timeline.js
@@ -1,3 +1,23 @@
+import Skeleton from "react-loading-skeleton";
+import usePhotos from "../hooks/use-photos";
+
+
 export default function Timeline() {
-    return <p>I am the timeline</p>;
+    // we need to get the logged in user's photos (hook)
+    const { photos } = usePhotos();
+    return (
+    <div className="container col-span-2">
+        {!photos ? (
+           <>
+                {[...new Array(4)].map((_, index) => (
+                    <Skeleton key={index} count={1} width={320} height={400} />
+                ))}
+            </>
+            ) : photos?.length > 0 ? (
+                photos.map((content) => <p key={content.docId}>{content.imageSrc}</p>)
+            ) : (
+                <p className="text-center text-2xl">Follow people to see photos!</p>
+           )}
+    </div>
+    );
 }
