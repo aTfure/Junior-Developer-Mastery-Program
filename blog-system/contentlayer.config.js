7cb5112cd110ad98032f03bae234b36496fec869
aTfure <atfure@gmail.com>
2024-04-22 13:27:23 +0300

    Update blog-one content and contentlayer configuration

--- blog-system/contentlayer.config.js
+++ blog-system/contentlayer.config.js
@@ -1,6 +1,48 @@
-import { makeSource } from "@contentlayer/source-files";
+import { defineDocumentType, makeSource } from "@contentlayer/source-files";
+
+const Blog = defineDocumentType(() => ({
+  name: "Blog",
+  filePathPattern: "**/**/*.mdx",
+  fields: {
+    title: {
+      type: "string",
+      required: true,
+    },
+    publishedAt: {
+      type: "date",
+      required: true,
+    },
+    updatedAt: {
+      type: "date",
+      required: true,
+    },
+    description: {
+      type: "string",
+      required: true,
+    },
+    image: { type: "image" },
+    // isPublished: { type: "string", required: true },
+    author: {
+      type: "string",
+      required: true,
+    },
+    tags: {
+      type: "list",
+      of: { type: "string" },
+    },
+  },
+
+  computedFields: {
+    url: {
+      type: "string",
+      resolve: (doc) =>
+        `/blogs/${doc._raw.flattenedPath.replace(/\\.[^/.]+$/, "")}`,
+    },
+  },
+}));
 
 export default makeSource({
   /* options */
   contentDirPath: "content",
+  documentTypes: [Blog],
 });
--- blog-system/src/app/page.js
+++ blog-system/src/app/page.js
@@ -1,9 +1,13 @@
-
+import { allBlogs } from "contentlayer/generated";
+import HomeCoverSection from "../components/Home/HomeCoverSection";
+import FeaturedPosts from "../components/Home/FeaturedPosts";
 
 export default function Home() {
+  // console.log(allBlogs);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Hello World
+      <HomeCoverSection blogs={allBlogs} />
+      <FeaturedPosts blogs={allBlogs} />
     </main>
-  )
+  );
 }
