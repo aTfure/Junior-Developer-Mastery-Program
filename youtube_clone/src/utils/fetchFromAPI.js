f3aa006ad3a2916b4e7816c9aaf10801589f46d2
aTfure <atfure@gmail.com>
2024-04-26 07:02:58 +0300

    Update YouTube API key and fetchFromAPI function

--- youtube_clone/src/utils/fetchFromAPI.js
+++ youtube_clone/src/utils/fetchFromAPI.js
@@ -7,17 +7,17 @@ const options = {
     maxResults: "50",
   },
   headers: {
-    "X-RapidAPI-Key": process.env.REACT_APP_RAPIDAPI_KEY,
+    "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
     "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
   },
 };
 
-try {
-  const response = await axios.request(options);
-  console.log(response.data);
-} catch (error) {
-  console.error(error);
-}
+// try {
+//   const response = await axios.request(options);
+//   console.log(response.data);
+// } catch (error) {
+//   console.error(error);
+// }
 
 export const fetchFromAPI = async (url) => {
   const { data } = await axios.get(`${BASE_URL}/${url}`, options);
