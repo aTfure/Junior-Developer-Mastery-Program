5e734528e0618e0f21cf0c152fc204ba9f808584
aTfure <atfure@gmail.com>
2024-04-26 11:35:34 +0300

    Refactor ChannelCard, ChannelDetail, and SearchBar components

--- youtube_clone/src/components/ChannelCard.jsx
+++ youtube_clone/src/components/ChannelCard.jsx
@@ -2,7 +2,7 @@ import { Box, CardContent, CardMedia, Typography } from "@mui/material";
 import { Link } from "react-router-dom";
 import { CheckCircle } from "@mui/icons-material";
 import { demoProfilePicture } from "../utils/constants";
-const ChannelCard = ({ channelDetail }) => (
+const ChannelCard = ({ channelDetail, marginTop }) => (
   <Box
     sx={{
       boxShadow: "none",
@@ -13,6 +13,7 @@ const ChannelCard = ({ channelDetail }) => (
       width: { xs: "356px", md: "320px" },
       height: "326px",
       margin: "auto",
+      marginTop,
     }}
   >
     <Link to={`/channel/${channelDetail?.id?.channelId}`}>
