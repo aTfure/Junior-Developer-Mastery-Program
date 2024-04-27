1eec00b71e1f4dfa8b76d87d400420f14bcc7d05
aTfure <atfure@gmail.com>
2024-04-27 06:40:57 +0300

    Refactor HR system code and add database configuration

--- human-resource-management-system/App.py
+++ human-resource-management-system/App.py
@@ -1,7 +1,12 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 
-app = Flask(__name__)
+app = Flask(__name__) # Creating an instance of the Flask class
+app.secret_key = 'secret_key'   # Secret key for the session
+
+# Configuring the database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 
 
 @app.route('/')
