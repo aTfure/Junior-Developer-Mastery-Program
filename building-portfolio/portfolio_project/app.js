76af75d7d27b865d0ee900476419bb3e4f7daa76
atfure@gmail.com <atfure>
2024-05-06 03:25:13 +0300

    Refactor CSS styles, update controls for navigation, and add new section for HR Management System

--- building-portfolio/portfolio_project/app.js
+++ building-portfolio/portfolio_project/app.js
@@ -1,43 +1,24 @@
 const sections = document.querySelectorAll(".section");
-const sectBtns = document.querySelectorAll(".controls");
-const sectBtn = document.querySelectorAll(".control");
-const allSections = document.querySelectorAll(".main-content");
+const sectBtns = document.querySelectorAll(".control"); // Changed from .controls to .control
+const allSections = document.querySelector(".main-content"); // Changed from querySelectorAll to querySelector
 
 function PageTransitions() {
   // Button click active class
-  for (let i = 0; i < sectBtn.length; i++) {
-    sectBtn[i].addEventListener("click", function () {
+  sectBtns.forEach((btn) => {
+    btn.addEventListener("click", function () {
       let currentBtn = document.querySelector(".active-btn");
-      if (currentBtn) {
-        currentBtn.classList.remove("active-btn");
-      }
+      currentBtn?.classList.remove("active-btn");
       this.classList.add("active-btn");
-    });
-  }
-
-  // Section Active
-  //   allSections.forEach((section) => {
-  //     section.addEventListener("click", (e) => {
-  //       console.log(e.target);
-  //     });
-  //   });
-  allSections.addEventListener("click", (e) => {
-    const id = e.target.dataset.id;
-    if (id) {
-      // remove active class from all sections
-      sectBtns.forEach((btn) => {
-        btn.classList.remove("active");
-      });
-      e.target.classList.add("active");
 
       // hide other sections
       sections.forEach((section) => {
         section.classList.remove("active");
       });
 
-      const element = document.getElementById(id);
+      // show the clicked section
+      const element = document.getElementById(this.dataset.id);
       element.classList.add("active");
-    }
+    });
   });
 }
 
