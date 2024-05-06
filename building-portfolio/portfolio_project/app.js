10567a4cb577c9bd181681489b7c1a78ab2ed085
aTfure <atfure@gmail.com>
2024-05-06 11:01:31 +0300

    Refactor app.js for improved code structure and performance

--- building-portfolio/portfolio_project/app.js
+++ building-portfolio/portfolio_project/app.js
@@ -1,25 +1,13 @@
-const sections = document.querySelectorAll(".section");
-const sectBtns = document.querySelectorAll(".control"); // Changed from .controls to .control
-const allSections = document.querySelector(".main-content"); // Changed from querySelectorAll to querySelector
-
-function PageTransitions() {
-  // Button click active class
-  sectBtns.forEach((btn) => {
-    btn.addEventListener("click", function () {
-      let currentBtn = document.querySelector(".active-btn");
-      currentBtn?.classList.remove("active-btn");
+(function () {
+  [...document.querySelectorAll(".control")].forEach((button) => {
+    button.addEventListener("click", function () {
+      document.querySelector(".active-btn").classList.remove("active-btn");
       this.classList.add("active-btn");
-
-      // hide other sections
-      sections.forEach((section) => {
-        section.classList.remove("active");
-      });
-
-      // show the clicked section
-      const element = document.getElementById(this.dataset.id);
-      element.classList.add("active");
+      document.querySelector(".active").classList.remove("active");
+      document.getElementById(button.dataset.id).classList.add("active");
     });
   });
-}
-
-PageTransitions();
+  document.querySelector(".theme-btn").addEventListener("click", () => {
+    document.body.classList.toggle("light-mode");
+  });
+})();
