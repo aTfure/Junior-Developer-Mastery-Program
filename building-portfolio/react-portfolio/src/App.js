911110f10be8c5053b3464060eaeba0ff0f9351b
aTfure <atfure@gmail.com>
2024-05-08 08:26:20 +0300

    Refactor CSS styles, update controls, and improve code structure for better performance

--- building-portfolio/react-portfolio/src/App.js
+++ building-portfolio/react-portfolio/src/App.js
@@ -3,13 +3,15 @@ import { Route, Routes } from 'react-router-dom'
 import Layout from './components/Layout/'
 import './App.css'
 import Home from './components/Home'
+import About from './components/About'
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />/
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
         </Route>
       </Routes>
     </>
